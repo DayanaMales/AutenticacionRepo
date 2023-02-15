358e6fa860574649ae369460eba699dabc461480
DayanaMales <j.males@intsuperior.edu.ec>
2023-02-14 19:29:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/autenticacion/Listar.java
@@ -0,0 +1,81 @@
+package com.example.autenticacion;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.autenticacion.Modal.Productos;
+import com.example.autenticacion.myAdapter.myAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class Listar extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private Button btnRegresarLista;
+    ArrayList<Productos> list;
+    DatabaseReference mDatabase;
+    myAdapter adapter;
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar);
+        btnRegresarLista=(Button)findViewById(R.id.btnRegresarLista);
+        btnRegresarLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                regresar();
+            }
+        });
+    {
+
+        };
+        recyclerView=findViewById(R.id.rvListaProductos);
+        mDatabase= FirebaseDatabase.getInstance().getReference("Productos");
+        list=new ArrayList<>();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter= new myAdapter(this,list);
+        recyclerView.setAdapter(adapter);
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for(DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    Productos producto=dataSnapshot.getValue(Productos.class);
+                    list.add(producto);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+    public void regresar() {
+        String regresar= btnRegresarLista.getText().toString();
+        Intent i = new Intent(this, MainActivity.class);
+        i.putExtra("regresar", regresar);
+        startActivity(i);
+    }
+}
